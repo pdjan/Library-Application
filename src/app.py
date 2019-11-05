51ce3406352bf7ae9d89e665bf4f3dea0ae76641
Predrag Nikolic <45166426+pdjan@users.noreply.github.com>
2019-11-05 09:55:19 +0100

    update ui

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 Application Library
 author: Predrag Nikolic github/pdjan
 date: nov 2019
-version 0.1.1
+version 0.1.2
 python: 3.7.4
 '''
 
@@ -15,7 +15,7 @@ import csv
 class Library:
     def __init__(self, master):
 
-        # Create empty database if it does not exist in the directory
+        # create empty database if it does not exist in the directory
         
         if not os.path.exists('data.db'):
             print('create database ...')
@@ -34,7 +34,25 @@ class Library:
         self.addbtn = ttk.Button(text="New book", width=15, command=self.add_book_dialog)
         self.addbtn.grid(row=0, column=0, sticky=W+N)
 
+        self.modbtn = ttk.Button(text="Edit book", width=15, command=self.edit_book_dialog)
+        self.modbtn.grid(row=1, column=0, sticky=W+N)
+
+        self.tree = ttk.Treeview(show="headings", height=20, column=4, selectmode="browse")
+        self.tree.grid(row=0, column=1, rowspan=20, sticky=N)
+        self.tree["columns"]=("one","two","tree","four")
+        self.tree.column("one", width=140)
+        self.tree.column("two", width=240)
+        self.tree.column("tree", width=100)
+        self.tree.column("four", width=100)
+        self.tree.heading("one", text='Author', anchor=N)
+        self.tree.heading("two", text='Title', anchor=N)
+        self.tree.heading("tree", text='Pages', anchor=N)
+        self.tree.heading("four", text='Date', anchor=N)        
+
     def add_book_dialog(self):
+        pass
+
+    def edit_book_dialog(self):
         pass
     
 root = Tk()
