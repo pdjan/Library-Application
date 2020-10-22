362fe4da71e869e8a8c558788f42cff7d42d55a1
Predrag Nikolic <45166426+pdjan@users.noreply.github.com>
2020-10-22 22:24:35 +0200

    theme applied

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 '''
 Application Library
 author: Predrag Nikolic github/pdjan
-date: May 2020
-version 1.0
+date: June 2020
+version 1.1
 python: 3.7.4
 '''
-# 
+# theme applied
 
 from tkinter import *
 from tkinter import ttk
+from ttkthemes import ThemedStyle
 import sqlite3
 import os.path
 import csv
@@ -75,6 +76,10 @@ class Library:
         try:
             self.msg["text"] = ""
             self.tl = Tk()
+            # Setting Theme
+            style = ThemedStyle(self.tl)
+            style.set_theme("plastik")
+            
             self.tl.title("Add book")
             self.tl.resizable(False, False)
             
@@ -83,32 +88,33 @@ class Library:
             y=root.winfo_rooty()+50
             self.tl.geometry('+%d+%d' % (x,y))
 
-            Label(self.tl,text='Author:').grid(row=0, column=0, sticky=W)
+            Label(self.tl,text='Author').grid(row=0, column=0, sticky=W+E)
             ne1var = StringVar()
-            ne1 = Entry(self.tl, textvariable=ne1var)
+            ne1 = ttk.Entry(self.tl, textvariable=ne1var)
             ne1.grid(row=0, column=1, sticky=W)
             ne1.insert(0,"")
 
-            Label(self.tl,text='Title:').grid(row=1, column=0, sticky=W)
+            Label(self.tl,text='Title').grid(row=1, column=0, sticky=W+E)
             ne2var = StringVar()
-            ne2 = Entry(self.tl, textvariable=ne2var)
+            ne2 = ttk.Entry(self.tl, textvariable=ne2var)
             ne2.grid(row=1, column=1, sticky=W)
             ne2.insert(0,"")
 
-            Label(self.tl,text='Pages:').grid(row=2, column=0, sticky=W)
+            Label(self.tl,text='Pages').grid(row=2, column=0, sticky=W+E)
             ne3var = StringVar()
-            ne3 = Entry(self.tl, textvariable=ne3var)            
+            ne3 = ttk.Entry(self.tl, textvariable=ne3var)            
             ne3.grid(row=2, column=1, sticky=W)
             ne3.insert(0,"")
 
-            Label(self.tl,text='Date:').grid(row=3, column=0, sticky=W)
+            Label(self.tl,text='Date').grid(row=3, column=0, sticky=W+E)
             ne4var = StringVar()
-            ne4 = Entry(self.tl, textvariable=ne4var)
+            ne4 = ttk.Entry(self.tl, textvariable=ne4var)
             ne4.grid(row=3, column=1, sticky=W)
             ne4.insert(0,"")            
 
             # Button calls function for executing sql command      
-            upbtn = Button(self.tl, bg="grey", fg="white", text= 'Add new book', command=lambda:self.add_book(ne1,ne2,ne3,ne4))
+            # upbtn = Button(self.tl, bg="grey", fg="white", text= 'Add new book', command=lambda:self.add_book(ne1,ne2,ne3,ne4))
+            upbtn = ttk.Button(self.tl, text= 'Add new book', command=lambda:self.add_book(ne1,ne2,ne3,ne4))
             upbtn.grid(row=5, column=0, sticky=W, pady=10, padx=10)
 
             self.config()
@@ -138,36 +144,43 @@ class Library:
                 
             self.tl = Tk()
             self.tl.title("Edit details")
+            
+            # Setting Theme
+            style = ThemedStyle(self.tl)
+            style.set_theme("plastik")
+            
             x = root.winfo_rootx()+120
             y = root.winfo_rooty()+50
             self.tl.geometry('%dx%d+%d+%d' % (380, 155, x, y))
             self.tl.resizable(False, False)
 
-            Label(self.tl,text='Author: ').grid(row=0, column=0, sticky=E+W)
-            new_author = Entry(self.tl, width=30)
+            Label(self.tl,text='Author ').grid(row=0, column=0, sticky=E+W)
+            new_author = ttk.Entry(self.tl, width=30)
             new_author.grid(row=0, column=1, sticky=W, padx=10)
             new_author.insert(0,_author)
 
-            Label(self.tl, text='Title: ').grid(row=1, column=0,sticky=E+W)
-            new_title = Entry(self.tl, width=30)
+            Label(self.tl, text='Title ').grid(row=1, column=0,sticky=E+W)
+            new_title = ttk.Entry(self.tl, width=30)
             new_title.grid(row=1, column=1, sticky=W, padx=10)
             new_title.insert(0,_title)
 
-            Label(self.tl, text='Pages:').grid(row=2, column=0,sticky=E+W)
-            new_pages = Entry(self.tl, width=30)
+            Label(self.tl, text='Pages').grid(row=2, column=0,sticky=E+W)
+            new_pages = ttk.Entry(self.tl, width=30)
             new_pages.grid(row=2, column=1, sticky=W, padx=10)
             new_pages.insert(0,_pages)
 
-            Label(self.tl, text='Date:').grid(row=3, column=0,sticky=E+W)
-            new_date = Entry(self.tl, width=30)
+            Label(self.tl, text='Date').grid(row=3, column=0,sticky=E+W)
+            new_date = ttk.Entry(self.tl, width=30)
             new_date.grid(row=3, column=1, sticky=W, padx=10)
             new_date.insert(0,_date)
                                         
-            upbtn = Button(self.tl, bg="grey", fg="white", text='Enter details',
-                           command=lambda:self.enter_changes(new_author,new_title,new_pages,new_date,name))
+            #upbtn = Button(self.tl, bg="grey", fg="white", text='Enter details',
+            #               command=lambda:self.enter_changes(new_author,new_title,new_pages,new_date,name))
+            upbtn = ttk.Button(self.tl, text='Enter details', command=lambda:self.enter_changes(new_author,new_title,new_pages,new_date,name))
             upbtn.grid(row=4, column=0, sticky=W, padx=10, pady=10)
 
-            dbtn = Button(self.tl, bg="grey", fg="white", text="Delete book", command=lambda:self.delete_book(name))
+            # dbtn = Button(self.tl, bg="grey", fg="white", text="Delete book", command=lambda:self.delete_book(name))
+            dbtn = ttk.Button(self.tl, text="Delete book", command=lambda:self.delete_book(name))
             dbtn.grid(row=4, column=1, sticky=W, padx=10, pady=10)
 
             conn.commit()
@@ -180,7 +193,7 @@ class Library:
             
         except IndexError as e:
             self.msg["text"] = "Select book to edit"
-			
+            
     
     def add_book(self,a,b,c,d):
         '''
@@ -202,7 +215,7 @@ class Library:
         
         self.config()
         self.update_list()
-		
+        
     def enter_changes(self,new_author,new_title,new_pages,new_date,name):
         '''
         Enter changes to database function
@@ -281,6 +294,11 @@ class Library:
 
     
 root = Tk()
+
+# Setting Theme
+style = ThemedStyle(root)
+style.set_theme("plastik")
+
 root.title("Library")
 application = Library(root)
 root.mainloop()
